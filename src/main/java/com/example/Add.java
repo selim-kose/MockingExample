33e82f0ce453241713b03f7d0a3becd00284fcbd
Selim Köse <selim-kose@live.com>
2023-02-15 16:49:50 +0100

    add test addition with 2 numbers

--- src/main/java/com/example/Add.java
+++ src/main/java/com/example/Add.java
@@ -1,4 +1,7 @@
 package com.example;
 
 public class Add {
+    public int addtwo(int i, int i1) {
+        return i+i1;
+    }
 }
--- src/test/java/com/example/AddTest.java
+++ src/test/java/com/example/AddTest.java
@@ -1,6 +1,19 @@
 package com.example;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AddTest {
 
     Add add = new Add();
+
+
+    @Test
+    @DisplayName("2 + 2 = 4")
+    void addingTwoNumbers(){
+        assertEquals(4,add.addtwo(2,2));
+    }
+
 }
